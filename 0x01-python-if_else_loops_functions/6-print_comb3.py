634eb7bfe0c976d7c1ee579c2a1dd052237d7b02
rolax4466 <rolaxcheruiyot325@gmail.com>
2023-10-31 17:23:47 +0300

    combinations

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
-for num1 in range(10):
-    for num2 in range(10):
-        if num2 > num1 and num1 != 8:
-            print("{}{}".format(num1, num2), end=", ")
-        elif num1 == 8 and num2 == 9:
-            print("{}{}".format(num1, num2))
-            break
+for i in range(0, 10):
+    for j in range(1, 10):
+        if i >= j:
+            continue
+        elif i == 8 and j == 9:
+            print("{}{}".format(i, j))
+        else:
+            print("{}{}, ".format(i, j), end="")
